036a9376365d92906838cf7c1395e3311171128f
Dwightnewell72yahoocom <dwightnewell72@yahoo.com>
2026-06-11 10:43:00 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,4 @@
-var CACHE = 'ut-report-v6';
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(['./manifest.json','./icon-192.png','./icon-512.png']); }));
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(e){
-  e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); }));
-  self.clients.claim();
-});
-self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request).then(function(r){ return r; }).catch(function(){ return caches.match(e.request); }));
-});
+var CACHE='ut-v5';
+self.addEventListener('install',function(e){self.skipWaiting();});
+self.addEventListener('activate',function(e){self.clients.claim();});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
