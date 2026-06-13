aa4e4e75ffeede1fa94570a5c7490bf88f8045a2
Dwightnewell72yahoocom <dwightnewell72@yahoo.com>
2026-06-12 18:39:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,46 @@
-var CACHE='ut-v34';
-self.addEventListener('install',function(e){self.skipWaiting();});
-self.addEventListener('activate',function(e){self.clients.claim();});
-self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
+var CACHE = 'ut-v36';
+var FILES = [
+  './',
+  './index.html',
+  './docx.js',
+  './jspdf.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        var clone = response.clone();
+        caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
